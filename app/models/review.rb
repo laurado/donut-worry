e6c358e0df06fb94dd5b4ec0422096303e08fae3
Colin Serafino <cserafino66@gmail.com>
2017-07-18 11:51:36 -0400

    add validation to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,10 @@
 class Review < ApplicationRecord
-  belongs_to :bakery
-  belongs_to :user
 
   validates :rating,
     presence: true,
     numericality: true,
     inclusion: { in: 1..5, message: 'must be a whole number between 1 and 5' }
+  validates :votes,
+    presence: true,
+    numericality: true
 end
