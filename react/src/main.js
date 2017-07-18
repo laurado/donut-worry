e569ec36c260cd230c8476b90edc9b10d68480cf
Colin Serafino <cserafino66@gmail.com>
2017-07-18 16:54:43 -0400

    Resolving merge conflicts.

--- react/src/main.js
+++ react/src/main.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
+<<<<<<< Updated upstream
     <h1>Boo yaa</h1>,
+=======
+    <h1>Hello from React</h1>,
+>>>>>>> Stashed changes
     document.getElementById('app')
   );
 })
