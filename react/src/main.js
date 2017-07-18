ec4ae82feb90a5da88627ee719c98081cc6f332a
Colin Serafino <cserafino66@gmail.com>
2017-07-18 16:59:46 -0400

    change ReactDom.render

--- react/src/main.js
+++ react/src/main.js
@@ -4,11 +4,7 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-<<<<<<< Updated upstream
-    <h1>Boo yaa</h1>,
-=======
     <h1>Hello from React</h1>,
->>>>>>> Stashed changes
     document.getElementById('app')
   );
 })
