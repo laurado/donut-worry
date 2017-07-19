5652bbc8e3d5d17b24c8d48e4818c6990f34d76c
Laura Do <laurado@Lauras-MacBook-Pro.local>
2017-07-19 11:52:23 -0400

    add react router and react router dom

---
+++ react/src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router, browserHistory, Route, IndexRoute } from 'react-router'
+
+const App = props => {
+  return (
+    <div>hello from App</div>
+  )
+}
+
+export default App;
