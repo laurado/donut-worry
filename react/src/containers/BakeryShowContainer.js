42e513831a1d340d9670983c7cd512880dfdaafd
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-07-20 16:37:26 -0400

    add fetch for bakery show page

--- react/src/containers/BakeryShowContainer.js
+++ react/src/containers/BakeryShowContainer.js
@@ -4,19 +4,32 @@ class BakeryShowContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      bakery: {}
+      bakery: {},
+      reviews: []
     }
   }
 
   componentDidMount() {
-    
+    let bakeryId = this.props.match.params.id
+    fetch(`/api/v1/bakeries/${bakeryId}`)
+    .then(response => response.json())
+    .then(body => {
+      this.setState({
+        bakery: body.bakery,
+        reviews: body.reviews
+      })
+    })
   }
 
   render() {
+    console.log(this.state.bakery)
+    console.log(this.state.reviews)
+
     return (
       <div>
         <hr></hr>
-        <h4>{this.state.bakery.address}, {this.state.bakery.city} {this.state.bakery.state}, {this.state.bakery.zip}</h4>
+        <h1>{this.state.bakery.name}</h1>
+        <h5>{this.state.bakery.address}, {this.state.bakery.city} {this.state.bakery.state}, {this.state.bakery.zip}</h5>
         <p>{this.state.bakery.description}</p>
       </div>
     )
