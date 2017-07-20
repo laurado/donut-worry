03c06fabef8e4237234e38af98e23cdcb105ba32
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-07-20 14:28:15 -0400

    told codeclimate to ignore bundle.js

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise"/
+  "@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -110,7 +111,6 @@ Devise.setup do |config|
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
   config.reset_password_within = 6.hours
-
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe User, type: :model do
   it { should_not have_valid(:last_name).when(nil, '') }
 
   it { should have_valid(:email).when('user@example.com', 'another@gmail.com') }
-  it { should_not have_valid(:email).when(
+  it do should_not have_valid(:email).when(
     nil,
     '',
     'duh',
-    'apple@sauce', 
+    'apple@sauce',
     'something.com'
-    )
-  }
+  )
+  end
 
   it 'has a matching password confirmation for the password' do
     user = User.new
