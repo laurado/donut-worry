e2c6cda3d4301e0582a3ea27cc1ae1fd9df11e4a
Jessica Titus <jtitus1195@gmail.com>
2017-07-24 17:21:05 -0400

    add bakeries create

--- app/controllers/bakeries_controller.rb
+++ app/controllers/bakeries_controller.rb
@@ -1,6 +1,10 @@
 class BakeriesController < ApplicationController
   def index
     render template: 'static_pages/index'
+    @shows =
+    if params[:search]
+        Bakery.search(params[:search]).order("created_at DESC")
+    end
   end
 
   def show
