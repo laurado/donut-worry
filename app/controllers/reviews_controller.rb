50aed8daccae8e4dc23a92e7bcdd1ac7b8ebf55f
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-07-25 11:05:24 -0400

    adding edit routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,34 @@ class ReviewsController < ApplicationController
     render template: 'static_pages/index'
   end
 
+  def edit
+    @bakery = Bakery.find(params[:bakery_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+      @review = Review.find(params[:id])
+      @review.update(review_params)
+      if @review.save
+        flash[:notice] = "Review successfully updated"
+        redirect_to bakery_path(@review.bakery)
+      else
+        flash[:alert] = "Unable to update. There was an error"
+        redirect_to edit_bakery_review_path(@review.show, @review)
+      end
+    end
+
+  def destroy
+    review = Review.find(params[:id])
+    @bakery = review.bakery
+    if Review.destroy(review.id)
+      flash[:notice] = "Your review has been deleted."
+    else
+      flash[:alert] = "Error deleting review."
+    end
+    redirect_to bakery_path
+  end
+
   def new
     @review = Review.new
     @bakery = Bakery.find(params[:bakery_id])
@@ -19,6 +47,7 @@ class ReviewsController < ApplicationController
     else
       render :new
     end
+
   end
 
   private
